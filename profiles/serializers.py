d601349bfe418019bec8469c4b74709a9a8cb899
GeorgeHazaka <hazaka.george@gmail.com>
2023-12-18 20:57:06 +0000

    Add post app and add a Post Model and Serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,10 +3,19 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    """
+    The SerializerMethodField is a read-only and it gets its value by calling a method on the 
+    serializer class, name get_<field_name>, which is <is_owner> in this case
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image',
+            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image', 'is_owner',
         ]
