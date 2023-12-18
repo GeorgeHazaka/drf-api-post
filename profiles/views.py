a3ebb9c04400391400586e3aa6c9788e2d23bd59
GeorgeHazaka <hazaka.george@gmail.com>
2023-12-18 20:20:10 +0000

    Add permissions and add profile detail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
-from rest_framework import generics
-from rest_framework.response import Response
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
@@ -7,8 +7,17 @@ from .serializers import ProfileSerializer
 class ProfileList(generics.ListCreateAPIView):
     """
     """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
