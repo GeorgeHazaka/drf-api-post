8551a3dc6aaaa25cc237de348868390a4f82e96c
GeorgeHazaka <hazaka.george@gmail.com>
2023-12-22 20:26:09 +0000

    Add posts_count to profiles

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Comment
+from comments.models import Comment
+from comment_likes.models import CommentLike
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -11,16 +12,27 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = CommentLike.objects.filter(
+                owner=user, comment=obj
+            ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Comment
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'post', 'created_at', 'updated_at', 'content'
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'post', 'created_at',
+            'updated_at', 'content', 'like_id',
         ]
 
 
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -9,6 +9,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -17,5 +18,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image', 'is_owner',
+            'id', 'owner', 'created_at',
+            'updated_at', 'name', 'content',
+            'image', 'is_owner', 'posts_count',
         ]
