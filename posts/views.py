77026251ffd7d3c8a861c8118b838047cceef9b1
GeorgeHazaka <hazaka.george@gmail.com>
2023-12-24 20:39:04 +0000

    Install django-filter and add filterset_fields to posts

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -9,6 +10,10 @@ class PostList(generics.ListCreateAPIView):
     """
     List posts or create a post if logged in
     The perform_create method associates the post with the logged in user.
+    In the filterset_fields:
+    likes__owner__profiles refers to user liked posts,
+    bookmarks__owner__profile refers to user bookmarked posts,
+    owner__profile refers to user posts.
     """
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -20,6 +25,12 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'likes__owner__profile',
+        'bookmarks__owner__profile',
+        'owner__profile',
     ]
     search_fields = [
         'owner__username',
